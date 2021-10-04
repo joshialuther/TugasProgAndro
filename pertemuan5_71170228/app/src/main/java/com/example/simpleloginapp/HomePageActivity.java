f6d4cf8ae8338f18ef722ecb27420e6f7d024af1
Joshia <joshia.luther@ti.ukdw.ac.id>
2021-10-04 20:53:03 +0700

    Pertemuan 5

---
+++ pertemuan5_71170228/app/src/main/java/com/example/simpleloginapp/HomePageActivity.java
@@ -0,0 +1,21 @@
+package com.example.simpleloginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class HomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+
+    }
+}
